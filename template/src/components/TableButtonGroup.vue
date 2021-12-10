6541a5ffa5f3865703925169f859d4f12404b5f3
MSI\sjkry505 <sjkry505@gmail.com>
2021-12-10 21:02:39 +0800

    add components

---
+++ template/src/components/TableButtonGroup.vue
@@ -0,0 +1,65 @@
+<template>
+  <div :class="$style.center">
+    <el-button type="primary" icon="el-icon-edit" :disabled="disable[0]" v-if="show[0]" @click="updateItem(scope.$index)"></el-button>
+    <el-button type="danger" :icon="isDelete ? 'el-icon-delete' : 'el-icon-setting'" :disabled="disable[1]" v-if="show[1]" @click="deleteItem(scope.$index)"></el-button>
+    <el-button type="warning" icon="el-icon-view" :disabled="disable[2]" v-if="show[2]" @click="watchItem(scope.$index)"></el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    scope: {
+      type: Object,
+    },
+    show: {
+      type: Array,
+      default: () => {
+        return [true, true, false]
+      }
+    },
+    disable: {
+      type: Array,
+      default: () => {
+        return [false, false, false]
+      }
+    },
+    isDelete: {
+      type: Boolean,
+      dafault: true
+    }
+  },
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    /**
+     * 按下修改按鈕
+     * @param {number} index 陣列索引值
+     */
+    updateItem(index) {
+      this.$emit('update', index)
+    },
+    /**
+     * 按下刪除按鈕
+     * @param {number} index 陣列索引值
+     */
+    deleteItem(index) {
+      this.$emit('delete', index)
+    },
+    /**
+     * 按下檢視按鈕
+     * @param {number} index 陣列索引值
+     */
+    watchItem(index) {
+      this.$emit('watch', index)
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+  @import "@/assets/css/custom.scss";
+</style>
--- template/src/services/componentLibrary.js
+++ template/src/services/componentLibrary.js
@@ -1,6 +1,7 @@
 import MainTitle from '@/components/MainTitle.vue'
 import PaginationBar from '@/components/PaginationBar'
 import FormButtonGroup from '@/components/FormButtonGroup'
+import TableButtonGroup from '@/components/TableButtonGroup'
 import EnableSwitch from '@/components/EnableSwitch'
 
-export { MainTitle, PaginationBar, FormButtonGroup, EnableSwitch }
+export { MainTitle, PaginationBar, FormButtonGroup, TableButtonGroup, EnableSwitch }
